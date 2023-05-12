feb7a64aae8d6c26b72b76e0db0a48558ac20fd7
cmathis94 <collin.mathis94@gmail.com>
2023-05-11 19:14:50 -0500

    changing from bulma to bootstrap :/

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const exp = require('constants');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
+const bootstrap = require('bootstrap');
 
 mongoose.connect('mongodb://localhost:27017/ms-quilt-guild');
 
@@ -18,6 +19,7 @@ const app = express();
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(__dirname + '/public'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
@@ -26,18 +28,26 @@ app.get('/', (req, res) => {
   res.render('home');
 });
 
-app.get('/about', async (req, res) => {
-  res.render('browse/about');
+app.get('/about-us', async (req, res) => {
+  res.render('about');
 });
 
 app.get('/contact-us', async (req, res) => {
-  res.render('/browse/contact');
+  res.render('contact');
+});
+
+app.get('/report-an-issue', async (req, res) => {
+  res.render('reportIssue');
 });
 
 app.get('/events', async (req, res) => {
-  res.render('/browse/events');
+  res.render('events');
 });
 
 app.get('/gallery', async (req, res) => {
-  res.render('/browse/gallery');
+  res.render('gallery');
+});
+
+app.listen(3000, () => {
+  console.log('On Port 3000');
 });
