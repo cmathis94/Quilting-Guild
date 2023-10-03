e7147e77c7ba31e8480c425e0a34847aafe71a8c
cmathis94 <collin.mathis94@gmail.com>
2023-10-03 14:03:00 -0500

    removed mongoose. I'm dumb

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const exp = require('constants');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
